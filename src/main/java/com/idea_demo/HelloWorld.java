7237796258b73f9e281422d32a028b2f5d263e5d
xmw <xmw@atguigu.com>
2021-08-25 23:36:14 +0800

    14 commit in IDEA

---
+++ src/main/java/com/idea_demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.idea_demo;
+
+/**
+ * 你好，世界
+ *
+ * @author DELL_
+ * @date 2021-08-25
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("hello, world!");
+    }
+}
