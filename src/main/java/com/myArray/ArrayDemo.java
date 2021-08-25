66442c2176b290ea73d7eaa24fc699a4324609eb
xmw <xmw@atguigu.com>
2021-08-25 20:38:22 +0800

    fourth commit in IDEA! Adding myArray module

---
+++ src/main/java/com/myArray/ArrayDemo.java
@@ -0,0 +1,20 @@
+package com.myArray;
+
+/**
+ * 数组的演示
+ *
+ * @author DELL_
+ * @date 2021-08-25
+ */
+
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int[] arr = {57, 16, 25, 39};
+        System.out.println(arr);
+        System.out.println(arr[0]);
+
+
+    }
+}
