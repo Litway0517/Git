628af6482537c18d1edac7e1adf8cce927fa1f78
Mingw0717 <kuailemingwei2018@outlook.com>
2021-08-26 16:19:50 +0800

    master new commit 1.5

--- src/main/java/com/myArray/ArrayDemo02.java
+++ src/main/java/com/myArray/ArrayDemo02.java
@@ -20,6 +20,7 @@ public class ArrayDemo02 {
         array.add("更新本地的user.name和user.email");
         array.add("master 更新一次");
         array.add("master 连续更新两次");
+        array.add("修改了user.name与user.email, 之后, GitHub开始统计commits与contributors");
 
 
         for (String s : array) {
