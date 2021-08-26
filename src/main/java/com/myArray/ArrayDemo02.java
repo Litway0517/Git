dcab3ea10ef691a6d9235608b8ebd220969a3ed7
Mingw0717 <kuailemingwei2018@outlook.com>
2021-08-26 16:43:20 +0800

    master new commit 1.8

--- src/main/java/com/myArray/ArrayDemo02.java
+++ src/main/java/com/myArray/ArrayDemo02.java
@@ -23,7 +23,9 @@ public class ArrayDemo02 {
         array.add("修改了user.name与user.email, 之后, GitHub开始统计commits与contributors");
         array.add("不正确 -> 更改了GitHub上面的Primary Email邮箱为xmw0517@gmail.com, 那么应该不会显示contributors. 结论: 实际上仍会显示");
 
-        array.add("更改了本地Git的user.name为XMW之后, 再次push应该不会显示contributors. ");
+        array.add("不正确 -> 更改了本地Git的user.name为XMW之后, 再次push应该不会显示contributors. 仍然会显示");
+        array.add("更改本地Git的user.email为xmw0517@163.com之后, 再次push应该不会显示contributors. ");
+        
 
 
         for (String s : array) {
