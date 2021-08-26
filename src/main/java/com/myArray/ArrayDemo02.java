b7f0a24e2e68ff3cad48745d1779b18d5ace637a
xmw <xmw@atguigu.com>
2021-08-26 15:33:53 +0800

    hot-fix commit 1

---
+++ src/main/java/com/myArray/ArrayDemo02.java
@@ -0,0 +1,28 @@
+package com.myArray;
+
+import java.util.ArrayList;
+
+/**
+ * 数组demo02
+ *
+ * @author DELL_
+ * @date 2021-08-26
+ */
+
+
+public class ArrayDemo02 {
+    public static void main(String[] args) {
+
+        // 创建集合
+        ArrayList<String> array = new ArrayList<String>();
+
+        array.add("左冷禅");
+        array.add("风清扬");
+        array.add("东方不败");
+        array.add("hot-fix 分支更新");
+
+        for (String s : array) {
+            System.out.println(s);
+        }
+    }
+}
