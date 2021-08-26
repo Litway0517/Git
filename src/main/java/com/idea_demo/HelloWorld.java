1ee6caf196ed776141f9ac4f7775e52a4948dd8e
Mingw0717 <xmw0517@gmail.com>
2021-08-27 00:44:49 +0800

    master new commit 2.1.15

--- src/main/java/com/idea_demo/HelloWorld.java
+++ src/main/java/com/idea_demo/HelloWorld.java
@@ -14,5 +14,10 @@ public class HelloWorld {
         System.out.println("gitee push test~");
         System.out.println("gitee web test");
         System.out.println("gitee push test~");
+
+        System.out.println("--------");
+
+        System.out.println("尝试更新本地库, 然后推送到GitHub, 之后再推送到Gitee. " + "\n" +
+                "切记: 不要在Gitee上修改代码, 仅在Git本地库与GitHub远程库之间pull/push. 适当将代码push到Gitee. ");
     }
 }
