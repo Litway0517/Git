9c11af22281ff20cbffed1ec6c7a90c324f6f8a3
Mingw0717-Dell <xmw0517@gmail.com>
2021-08-27 13:17:03 +0800

    master new commit 2.1.16

--- src/main/java/com/idea_demo/HelloWorld.java
+++ src/main/java/com/idea_demo/HelloWorld.java
@@ -19,5 +19,9 @@ public class HelloWorld {
 
         System.out.println("尝试更新本地库, 然后推送到GitHub, 之后再推送到Gitee. " + "\n" +
                 "切记: 不要在Gitee上修改代码, 仅在Git本地库与GitHub远程库之间pull/push. 适当将代码push到Gitee. ");
+
+        System.out.println("--------");
+
+        System.out.println("修改用户名测试");
     }
 }
